1c93b40a7e0490ba4f2cf9a7b9e0a9606c23ed6a
JIn Cheng Wei <waynechin910256@gmail.com>
2020-10-11 01:24:47 +0800

    Create: Public Layout folder

---
+++ frontend/src/components/layout/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    
+</template>
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style lang="stylus">
+
+</style>
