c9402b1bbba8e6c0994e9ee9386542e99000cc63
JInChengWei <waynechin910256@gmail.com>
2020-10-12 02:09:57 +0800

    Plugin: Use Vuetify & Add temp logo

--- frontend/src/components/layout/Header.vue
+++ frontend/src/components/layout/Header.vue
@@ -1,17 +1,36 @@
 <template>
-    
-</template>
+  <div class="container">
+    <img src="../../../public/img/icons/logo.jpg" alt="logo" class="logo" />
 
+    <div class="bookmark-switch">
+      <button>Switch</button>
+    </div>
+
+    <div class="header-button-group">
+      <button>Don't Disturb</button>
+      <button>Latest Updates</button>
+      <button>Layout Setting</button>
+      <button>Sign In</button>
+    </div>
+  </div>
+</template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+export default class Header extends Vue {
+
 }
 </script>
 
-<style lang="stylus">
+<style>
+.container {
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px 15px;
+}
 
+img {
+  width: 200px;
+}
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Header></Header>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Header from "@/components/layout/Header.vue";
 
 @Component({
   components: {
-    HelloWorld
+    Header
   }
 })
 export default class Home extends Vue {}
