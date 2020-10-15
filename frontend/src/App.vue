611ebac44bd7c056599e55062dfe644f52883165
JInChengWei <waynechin910256@gmail.com>
2020-10-15 16:16:08 +0800

    Header style draft

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,51 +1,22 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app flat>
+      <PublicHeader></PublicHeader>
     </v-app-bar>
 
     <v-main>
-      <CheckFile></CheckFile>
+      content
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import CheckFile from "./components/CheckFile.vue";
+import PublicHeader from "./components/PublicHeader.vue";
 
 @Component({
   components: {
-    CheckFile
+    PublicHeader,
   }
 })
 export default class App extends Vue {}
