c522678d2641acba04b502e47100dafd15a1da09
JIn Cheng Wei <waynechin910256@gmail.com>
2020-10-18 15:25:47 +0800

    Upload the built files under ./dist.

---
+++ frontend/dist/precache-manifest.e0751a19490af479e87d94b3f8fdb7bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4f85a652ee2b435964",
+    "url": "css/app.9551cd0d.css"
+  },
+  {
+    "revision": "ad3cb02a96d7c08527f1",
+    "url": "css/chunk-vendors.2468633d.css"
+  },
+  {
+    "revision": "6c724a1de1655d41f1bb523cdf2f1055",
+    "url": "index.html"
+  },
+  {
+    "revision": "8e4f85a652ee2b435964",
+    "url": "js/app.7d78c702.js"
+  },
+  {
+    "revision": "ad3cb02a96d7c08527f1",
+    "url": "js/chunk-vendors.8dfe245c.js"
+  },
+  {
+    "revision": "4b14c64efaf846819b9a229b4193c8b7",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
