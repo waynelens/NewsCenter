fd7defd9275218c641d0e09d2724c2ae3d7030e7
JIn Cheng Wei <waynechin910256@gmail.com>
2020-10-23 18:53:24 +0800

    DB first seed data test -- Success.

--- backend/NewCenter/Migrations/20201023103029_SeedInsertTest.cs
+++ backend/NewCenter/Migrations/20201023103029_SeedInsertTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 
 namespace NewCenter.Migrations
 {
@@ -6,7 +7,11 @@ namespace NewCenter.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "CreatTime","EditTime","IsDelete","RefCreatorId","RefEditorId","Name","Email","Password","Avator","RefPermissionId" },
+                values: new object[] {DateTime.Now,null,false,1,null,"Admin","Admin@gmail.com","Admin0208", "https://avataaars.io/?avatarStyle=Transparent&topType=LongHairDreads&accessoriesType=Round&hairColor=SilverGray&facialHairType=BeardLight&facialHairColor=BrownDark&clotheType=CollarSweater&clotheColor=Gray01&eyeType=Dizzy&eyebrowType=UpDownNatural&mouthType=Default&skinColor=DarkBrown'", 1}
+            );
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
